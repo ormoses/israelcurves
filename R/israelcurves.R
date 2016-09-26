826b3f39a3a23d03565af959ed5ce4cb2708b7ac
Or Moses <orm@inbal.co.il>
2016-09-26 17:08:28 +0300

    package description

---
+++ R/israelcurves.R
@@ -0,0 +1,12 @@
+#' israelcurves: calculating curves for the Israeli sovereign bonds.
+#'
+#' The package uses Nelson Siegel and Svensson interpolation methods to calculate
+#' historical zero curves for the Israeli sovereign bond market.
+#'
+#' @section israelcurves functions:
+#' bond
+#' calc_all
+#'
+#' @docType package
+#' @name israelcurves
+NULL
