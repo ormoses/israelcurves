c0d04568233ec7ad67213416393b690253431a3f
Or Moses <orm@inbal.co.il>
2016-10-06 15:57:03 +0300

    import from

--- R/check_boi_curve.R
+++ R/check_boi_curve.R
@@ -5,6 +5,7 @@
 #' @param result A list with the results of the package calculation
 #' @param boi_file a csv file with the zero curves data from the bank of israel
 #' @importFrom utils read.csv
+#' @importFrom dplyr filter
 #' @return A list with the model data, BOI data and differences
 
 check_boi <- function(result,boi_file) {
@@ -17,7 +18,7 @@ check_boi <- function(result,boi_file) {
   #get end of month data from result
   model <- result$model
   a <- result$result
-  a <- dplyr::filter(a,date %in% BOI$date)
+  a <- filter(a,date %in% BOI$date)
   #calculate the curves for each date
   if (model=="NS") {
     num <- 4
@@ -29,7 +30,7 @@ check_boi <- function(result,boi_file) {
   model_yields <- data.frame(date=format(a$date,"%Y-%m-%d"),model_yields*100)
   model_yields$date <- as.Date(model_yields$date)
   names(model_yields)[2:9] <- terms
-  BOI <- dplyr::filter(BOI,date %in% model_yields$date)
+  BOI <- filter(BOI,date %in% model_yields$date)
   dif <- BOI
   dif[2:9] <- BOI[2:9]-model_yields[2:9]
   maxs <- apply(dif,1,function(x) as.numeric(max(x[2:9])))
