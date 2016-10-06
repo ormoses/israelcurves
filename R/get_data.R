25e1ce3d98009eb2cc11145e7dac02ebb38170c0
Or Moses <orm@inbal.co.il>
2016-10-06 16:17:34 +0300

    added end date

--- R/get_data.R
+++ R/get_data.R
@@ -5,6 +5,7 @@
 #' @param srch_name A string. A bloomberg saved SRCH name.
 #' @param bond_list A list of bond objects.
 #' @param start_date A date to start getting the data from.
+#' @param end_date A date. Last date to get data from.
 #' @return A dataframe with the following columns:
 #' \itemize{
 #'    \item date
@@ -18,17 +19,17 @@
 #' from a Bloomberg SRCH.
 #' @importFrom dplyr mutate arrange select %>%
 #' @export
-get_daily_data <- function(srch_name, bond_list, start_date) {
+get_daily_data <- function(srch_name, bond_list, start_date, end_date=NULL) {
     # Connect to bloomberg
     con <- Rblpapi::blpConnect()
     # get tickers list from a bloomberg custom SRCH
     x <- as.character(Rblpapi::bsrch(paste0("FI:", srch_name))$id)
     x <- gsub(" Corp", "@TASE Corp", x)
     # get bonds main data
-    bond_names <- x %>% Rblpapi::bdp(c("ID_BB", "SERIES")) %>% mutate(ID_BB = paste0(substr(ID_BB, 1, nchar(ID_BB) - 
+    bond_names <- x %>% Rblpapi::bdp(c("ID_BB", "SERIES")) %>% mutate(ID_BB = paste0(substr(ID_BB, 1, nchar(ID_BB) -
         1), "@TASE Corp"))
     # get bonds daily data
-    daily_data <- x %>% Rblpapi::bdh(c("PX_LAST", "VOLUME"), start.date = start_date)
+    daily_data <- x %>% Rblpapi::bdh(c("PX_LAST", "VOLUME"), start.date = start_date, end.date=end_date)
     # Disconnect bloomberg
     Rblpapi::blpDisconnect(con)
     # change the names of the lists to the series name
@@ -41,8 +42,8 @@ get_daily_data <- function(srch_name, bond_list, start_date) {
     daily_data <- do.call(rbind, daily_data)
     # Add maturity and arrange by date and maturiy
     maturs <- apply(daily_data, 1, function(x) bond_by_name(bond_list, x[4])$maturity)
-    daily_data <- daily_data %>% mutate(maturity = as.Date(maturs, origin = "1970-01-01")) %>% arrange(date, 
-        maturity) %>% mutate(term = as.numeric((maturity - date)/365)) %>% select(date, market_price = PX_LAST, 
+    daily_data <- daily_data %>% mutate(maturity = as.Date(maturs, origin = "1970-01-01")) %>% arrange(date,
+        maturity) %>% mutate(term = as.numeric((maturity - date)/365)) %>% select(date, market_price = PX_LAST,
         trade_volume = VOLUME, name:term)
     return(daily_data)
 }
