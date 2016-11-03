690f667915a83bd9e928526fff8927c0c93f4ca0
Or Moses <orm@inbal.co.il>
2016-11-03 16:05:45 +0200

    minor changes

--- R/main.R
+++ R/main.R
@@ -16,13 +16,13 @@ build_curves <- function(srch_name,start_date,end_date=NULL,min_obs=6,
                          model="NS",adj_dur=TRUE,adj_vol=FALSE,max_vol=NULL,ex_day=NULL) {
   # Get the main bond data - bond_list
   bond_list <- create_all_bonds(srch_name)
-  cat("Got all bonds")
+  cat("===== Got all bonds ===== \n")
   # Get the daily data for the bonds
   daily_data <- get_daily_data(srch_name,bond_list,start_date,end_date = end_date)
-  cat("Got the daily data")
+  cat("===== Got the daily data ===== \n")
   # Get CPI list
-  cat <- get_cpi_data()
-  print("Got the CPI list")
+  cpi_list <- get_cpi_data()
+  cat("===== Got the CPI list ===== \n")
   # Compute the results
   result <- make_params_for_all_dates(bond_list, daily_data, cpi_list, min_obs, model, adj_dur, adj_vol, max_vol, ex_day)
   return(result)
